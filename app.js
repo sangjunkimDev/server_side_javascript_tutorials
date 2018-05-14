c9b6be4b3cdbd178c2f76f01add32660c54c03fe
unknown <sangjun.dev@gmail.com>
2018-05-15 01:09:20 +0900

    Add js : practice express module

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+var app = express();
+app.get('/', function(req, res){
+  res.send('hello home page');
+});
+app.get('/login', function(req, res){
+  res.send('login please');
+});
+
+
+app.listen(3000, function(){
+  console.log('Connected 3000 port!');
+});
